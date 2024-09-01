625d2453466ad4f11c43a8ccca3af998fbe2617a
Andres <andrespardo5151@gmail.com>
2024-09-01 07:13:29 -0500

    Doc

--- dic/Configuracion del usuario/controlador/ProfileControllertxt.php
+++ dic/Configuracion del usuario/controlador/ProfileControllertxt.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\File;
 class ProfileController extends Controller
 {
     /**
-     * metood edit, se encarga de mostrar la vista de edición de perfil.
+     * metodo edit, se encarga de mostrar la vista de edición de perfil.
      */
     public function edit(Request $request): View
     {
@@ -29,11 +29,9 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-      //dd($request->hasFile('image_path'));
-        
- 
+        //dd($request->hasFile('image_path'));       
 
-         // Obtenemos el usuario identificado
+        // Obtenemos el usuario identificado
         $user = Auth::user();
 
         //obtenemos los datos del fomulario 
@@ -42,44 +40,44 @@ class ProfileController extends Controller
         $surname = $request->input('surname');
         $nick = $request->input('nick');
         $email = $request->input('email');
-        $phone= $request->input('phone');
+        $phone = $request->input('phone');
 
         // Validamos los datos del formulario con los datos de la base de datos
         $request->validate([
-            'name' => ['required', 'string', 'alpha','max:255'],
-            'surname' => ['required', 'string','alpha', 'max:255'],
-            'nick' => ['required', 'string', 'max:255', 'unique:users,nick,'.$id],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:users,email,'.$id],
+            'name' => ['required', 'string', 'alpha', 'max:255'],
+            'surname' => ['required', 'string', 'alpha', 'max:255'],
+            'nick' => ['required', 'string', 'max:255', 'unique:users,nick,' . $id],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:users,email,' . $id],
             'phone' => ['required', 'string', 'max:20'],
             'image_path' => 'nullable|image|mimes:jpg,jpeg,png,gif|max:2048',
-        
+
         ]);
 
-      
-     // Capturamos los datos y los guardamos en la base de datos
+
+        // Capturamos los datos y los guardamos en la base de datos
         $user->name = $name;
         $user->surname = $surname;
         $user->nick = $nick;
         $user->email = $email;
         $user->phone = $phone;
-       
+
 
         //Subir la imagen
-       $image_path = $request->file('image_path');
-    if  ($image_path) {
+        $image_path = $request->file('image_path');
+        if ($image_path) {
 
-        // Poner nombre unico
-      $image_path_name = time().'_'.$image_path->getClientOriginalName();
+            // Poner nombre unico
+            $image_path_name = time() . '_' . $image_path->getClientOriginalName();
 
-        // Guardar en la carpeta storage (storage/app/users)
-        Storage::disk('users')->put($image_path_name, \File::get($image_path));
+            // Guardar en la carpeta storage (storage/app/users)
+            Storage::disk('users')->put($image_path_name, \File::get($image_path));
 
-        // Seteo el nombre de la imagen en el objeto
-        $user->image_path= $image_path_name;
-    }
-     
+            // Seteo el nombre de la imagen en el objeto
+            $user->image_path = $image_path_name;
+        }
 
-          // Guardamos los datos
+
+        // Guardamos los datos
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
@@ -90,8 +88,9 @@ class ProfileController extends Controller
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
-//Metodo para obtener la imagen del usuario
-    public function getImage($filename){
+    //Metodo para obtener la imagen del usuario
+    public function getImage($filename)
+    {
         $file = Storage::disk('users')->get($filename);
         return new Response($file, 200);
     }
@@ -116,4 +115,56 @@ class ProfileController extends Controller
 
         return Redirect::to('/');
     }
+}
+
+
+// Diccionario de terminos utilizadios para esta practica 
+
+/*
+
+namespace App\Http\Controllers;
+namescape = Espacio de nombres
+use = Se utiliza para importar las calses que se necesitan
+App = Es el nombre de la carpeta principal de la aplicacion
+Iluuminate = Es el nombre de la carpeta principal del framework
+use App\Http\Requests\ProfileUpdateRequest; = Indica que se importatra la clase ProfileUpdateRequest
+use Illuminate\Http\RedirectResponse; = Indica que se importara la clase RedirectResponse
+use Illuminate\Http\Request; = Indica que se importara la clase Request
+use Illuminate\Http\Response; = Indica que se importara la clase Response
+use Illuminate\Support\Facades\Auth; 61 = Indica que se importara la clase Auth
+use Illuminate\Support\Facades\Redirect; = Indica que se importara la clase Redirect
+use Illuminate\View\View; = Indica que se importara la clase View
+use Illuminate\Support\Facades\Storage; = Indica que se importara la clase Storage
+use Illuminate\Support\Facades\File; = Indica que se importara la clase File
+
+
+
+/*Se crea el metodo edit que recibe un request y devuelve una vista*/
+//Se crea la clase ProfileController que extiende de Controller
+class ProfileControllertxt extends Controller
+{
+
+    public function edit(Request $request): View // = Indica que el metodo edit recibe un request y devuelve una vista
+    {
+        return view('profile.edit', [
+            'user' => $request->user(), // = Devuelve la vista profile.edit y el usuario
+        ]);
+    }
+
+
+/*
+Request = Es una clase que se utiliza para obtener los datos de la solicitud.
+$request = Es una variable que se utiliza para obtener los datos de la solicitud.
+View = Es una clase que se utiliza para devolver una vista.
+view = Es una funcion que se utiliza para devolver una vista.
+'user' = Es una variable que se utiliza para almacenar los datos del usuario.
+user() = Es una funcion que se utiliza para obtener los datos del usuario. 
+
+
+ */
+
+
+/**/
+
+
 }
