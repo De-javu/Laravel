13b1af7916a81dadf99084b8fd4de3395c227908
Andres <andrespardo5151@gmail.com>
2024-10-14 07:29:07 -0500

    perfil rutas

--- routes/web.php
+++ routes/web.php
@@ -31,31 +31,34 @@ Route::get('/dashboard', function () {
 
 // Agregar la ruta para 'profile'
 Route::middleware('auth')->group(function () {
+
     // Agregar las rutas para la configuracion de perfil'
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/profile/avatar/{filename}', [ProfileController::class, 'getImage'])->name('profile.avatar');
+
     // Agregar la ruta para 'image'
     Route::get('/image', [ImageController::class, 'create'])->name('image.create');
     Route::patch('/image/save', [ImageController::class, 'save'])->name('image.save');
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/image/file/{filename}', [HomeController::class, 'getImage'])->name('image.file');
     Route::get('/image/{id}', [ImageController::class, 'detail'])->name('image.detail');
+
     // Agregar la ruta para 'comment'
     Route::patch('/comment/store', [CommentController::class, 'store'])->name('comment.store');
     Route::get('/comment/destroy/{id}', [CommentController::class, 'destroy'])->name('comment.destroy');
     Route::get('/comment/{id}/edit', [CommentController::class, 'edit'])->name('comment.edit');
     Route::put('/comment/{id}', [CommentController::class, 'update'])->name('comment.update');
+
     // Agregar la ruta para 'like'
     Route::get('/like/{image_id}', [LikeController::class, 'like'])->name('like.save');
     Route::get('/deslike/{image_id}', [LikeController::class, 'deslike'])->name('deslike.delete');
-    Route::get('/like', [LikeController::class, 'index'])->name('like.index');
-
+    Route::get('/like', [LikeController::class, 'index'])->name('like.index');    
     
-// Agregar la ruta para 'settings'
-Route::get('/perfil/{id}', [SettingsController::class, 'perfil'])->name('settings.perfil');
-});
+    // Agregar la ruta para 'settings'
+    Route::get('/perfil/{id}', [SettingsController::class, 'perfil'])->name('settings.perfil');
+    });
 
 
 
